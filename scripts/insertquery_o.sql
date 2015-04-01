c749868ea8a9b0fd173c67146201a9832933f8fa
Mover <rmuyinda@newwavetech.co.ug>
2015-04-01 16:32:00 +0300

    adding sql to staging area

---
+++ scripts/insertquery_o.sql
@@ -0,0 +1,46 @@
+﻿INSERT INTO `tmis`.`training` (`code`,`notes`, `type`) 
+ VALUES
+  ('P001','Pre-Service (TDMS)','education'),
+  ('P001','Pre-Service (TDMS)','education'),
+  ('P002','In-Service (TDMS)','education'),
+  ('P003','Upgrading Full-Time','education'),
+  ('P004','Upgrading Part-Time (In Service)','education'),
+  ('P005',' Training in Sanitation  Promotion','education'),
+  ('P006',' Training in physical education and sports (Certificate)','education'),
+  ('P007','Training in physical education and sports (Diploma)','education'),
+  ('P008','Training in physical education and sports (Degree)','education'),
+  ('S001','Upgrading Full-Time','education'),
+  ('S003',' Training in Sanitation  Promotion','education'),
+  ('S004','Training in physical education and sports (Certificate)','education'),
+  ('S005',' Training in physical education and sports (Diploma)','education'),
+  ('S006','Training in physical education and sports (Degree)','education'),
+  ('B001','In-Service','education'),
+  ('B002','Upgrading','education'), 
+  ('B003','Higher Education (full time)','education'), 
+  ('B004','Higher Education (part time)','education'), 
+  ('B005','Training in physical education and sports','education');
+
+  
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
