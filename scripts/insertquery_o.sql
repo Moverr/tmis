541aa8956b49ca8dea8d7f561bed17f38ea72fe2
Mover <rmuyinda@newwavetech.co.ug>
2015-04-01 16:39:02 +0300

    updated SQL version

--- scripts/insertquery_o.sql
+++ scripts/insertquery_o.sql
@@ -1,24 +1,24 @@
 ﻿INSERT INTO `tmis`.`training` (`code`,`notes`, `type`) 
  VALUES
-  ('P001','Pre-Service (TDMS)','education'),
-  ('P001','Pre-Service (TDMS)','education'),
-  ('P002','In-Service (TDMS)','education'),
-  ('P003','Upgrading Full-Time','education'),
-  ('P004','Upgrading Part-Time (In Service)','education'),
-  ('P005',' Training in Sanitation  Promotion','education'),
-  ('P006',' Training in physical education and sports (Certificate)','education'),
-  ('P007','Training in physical education and sports (Diploma)','education'),
-  ('P008','Training in physical education and sports (Degree)','education'),
-  ('S001','Upgrading Full-Time','education'),
-  ('S003',' Training in Sanitation  Promotion','education'),
-  ('S004','Training in physical education and sports (Certificate)','education'),
-  ('S005',' Training in physical education and sports (Diploma)','education'),
-  ('S006','Training in physical education and sports (Degree)','education'),
-  ('B001','In-Service','education'),
-  ('B002','Upgrading','education'), 
-  ('B003','Higher Education (full time)','education'), 
-  ('B004','Higher Education (part time)','education'), 
-  ('B005','Training in physical education and sports','education');
+  ('P001','Pre-Service (TDMS)','educationala'),
+  ('P001','Pre-Service (TDMS)','educational'),
+  ('P002','In-Service (TDMS)','educational'),
+  ('P003','Upgrading Full-Time','educational'),
+  ('P004','Upgrading Part-Time (In Service)','educational'),
+  ('P005','Training in Sanitation  Promotion','educational'),
+  ('P006','Training in physical educational and sports (Certificate)','educational'),
+  ('P007','Training in physical educational and sports (Diploma)','educational'),
+  ('P008','Training in physical educational and sports (Degree)','educational'),
+  ('S001','Upgrading Full-Time','educational'),
+  ('S003',' Training in Sanitation  Promotion','educational'),
+  ('S004','Training in physical educational and sports (Certificate)','educational'),
+  ('S005',' Training in physical educational and sports (Diploma)','educational'),
+  ('S006','Training in physical educational and sports (Degree)','educational'),
+  ('B001','In-Service','educational'),
+  ('B002','Upgrading','educational'), 
+  ('B003','Higher educational (full time)','educational'), 
+  ('B004','Higher educational (part time)','educational'), 
+  ('B005','Training in physical educational and sports','educational');
 
   
 
